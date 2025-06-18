76756c86ea64be4f6d57ed828fc3ccf3cea8e604
sanket-pilane <lalitpilane7@gmail.com>
2025-06-18 14:02:03 +0530

    hero done

---
+++ components/ui/BentoGrid.tsx
@@ -0,0 +1,84 @@
+import { cn } from "@/lib/utils";
+
+export const BentoGrid = ({
+    className,
+    children,
+}: {
+    className?: string;
+    children?: React.ReactNode;
+}) => {
+    return (
+        <div
+            className={cn(
+                "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto3",
+                className,
+            )}
+        >
+            {children}
+        </div>
+    );
+};
+
+export const BentoGridItem = ({
+    className,
+    title,
+    description,
+    id,
+    img,
+    imgClassName,
+    titleClassName,
+    spareImg
+}: {
+    className?: string;
+    title?: string | React.ReactNode;
+    description?: string | React.ReactNode;
+    header?: React.ReactNode;
+    icon?: React.ReactNode;
+    id?: number,
+    img?: string,
+    imgClassName?: string,
+    titleClassName?: string,
+    spareImg?: string,
+}) => {
+    return (
+        <div
+            className={cn(
+                "group/bento relative shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-3xl  border bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none ",
+                className,
+            )}
+            style={{
+                background: "rgb(4,7,29)",
+                backgroundColor:
+                    "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+
+            }}
+
+        >
+
+            <div className={`${id === 6} && 'flex justify-center h-full' `}>
+                <div className="h-full w-full absolute">
+                    {img && (
+                        <img src={img} alt={img} className={cn(imgClassName, 'object-cover')} />
+                    )}
+                </div>
+
+                <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+                    {spareImg && (
+                        <img src={spareImg} alt={spareImg} className={'object-conver object-center w-full h-full'} />
+                    )}
+
+                </div>
+            </div>
+
+            <div className="transition duration-200 group-hover/bento:translate-x-2">
+
+                <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+                    {title}
+                </div>
+                <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+                    {description}
+                </div>
+            </div>
+        </div>
+    );
+};
